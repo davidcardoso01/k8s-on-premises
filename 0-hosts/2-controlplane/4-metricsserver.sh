86f55e56cefb06af97e90797b9063255f2f9e11a
David Rafael Cardoso <davidcardoso01@gmail.com>
2024-03-17 21:46:59 -0300

    Criando script que instala o metrics server no kubernetes

---
+++ 0-hosts/2-controlplane/4-metricsserver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+helm repo update
+helm upgrade --install metrics-server metrics-server/metrics-server --namespace kube-system --set args={--kubelet-insecure-tls}
