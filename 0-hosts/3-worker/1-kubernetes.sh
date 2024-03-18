52e0ed6b6305da517dcf143e5ad6a1144f9754c1
David Rafael Cardoso <davidcardoso01@gmail.com>
2024-03-17 21:47:08 -0300

    Criando script que cria e configura o worker

---
+++ 0-hosts/3-worker/1-kubernetes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "########################################"
+echo "###   edite esse arquivo e inclua   ###"
+echo "###   o comando que você salvou     ###"
+echo "###   anteriormente.                ###"
+echo "########################################"
+
+echo "Exemplo: sudo kubeadm join 192.168.0.203:6443 --token 0nzday.907oet4n3v4aburb \
+ --discovery-token-ca-cert-hash sha256:8a25937bd9a96f3acdfb0eacad2c06f680ddd2de8eed7c13334e7327ff70bcdf"
+
+ #sudo kubeadm join <IP>:<PORT> --token <TOKEN> --discovery-token-ca-cert-hash sha256:<HASH>
